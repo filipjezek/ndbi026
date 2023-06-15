1c0a40be11b5ab33d951a89e10a886bd8a2a49e3
Filip Ježek <filip.jezek@gmail.com>
2023-06-15 17:41:43 +0200

    add indices

---
+++ 1.1-schema-indices.sql
@@ -0,0 +1,12 @@
+-- author: Filip Ježek (44432378)
+
+-- foreign key indices
+
+CREATE INDEX IX_instanceof ON subject_instances(instanceof);
+CREATE INDEX IX_student ON students_programmes(student);
+CREATE INDEX IX_programme ON students_programmes(programme);
+CREATE INDEX IX_subject ON students_subjects(subject);
+CREATE INDEX IX_student ON students_subjects(student);
+CREATE INDEX IX_teacher ON teachers_degrees(teacher);
+CREATE INDEX IX_teacher ON teachers_subjects(teacher);
+CREATE INDEX IX_subject ON teachers_subjects(subject);

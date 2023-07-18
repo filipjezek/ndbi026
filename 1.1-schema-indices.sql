ad8df9e1dc91b8d43752b5ff2b7b05faa06e94dc
Filip Ježek <filip.jezek@gmail.com>
2023-07-19 01:27:29 +0200

    remove redundant indices

--- 1.1-schema-indices.sql
+++ 1.1-schema-indices.sql
@@ -5,12 +5,6 @@
 CREATE INDEX IX_instanceof ON subject_instances(instanceof);
 CREATE INDEX IX_student ON students_programmes(student);
 CREATE INDEX IX_programme ON students_programmes(programme);
-CREATE INDEX IX_subject ON programme_required(subject);
-CREATE INDEX IX_programme ON programme_required(programme);
-CREATE INDEX IX_subject ON students_subjects(subject);
-CREATE INDEX IX_student ON students_subjects(student);
-CREATE INDEX IX_teacher ON teachers_subjects(teacher);
-CREATE INDEX IX_subject ON teachers_subjects(subject);
 
 -- used in views and triggers
 CREATE INDEX IX_year ON subject_instances(year);
